211228b3a889a71233e065a5d978e148c481c480
Aaron Conway <aaronconwayphd@gmail.com>
2021-05-31 11:28:48 -0400

    fixed duplicate hypotension item

--- index.js
+++ index.js
@@ -1,4 +1,372 @@
-var surveyJSON = {"pages":[{"name":"page1","elements":[{"type":"rating","name":"riskCr","title":"I am confident that I am able to use information about cardiorespiratory reserve to assess risk for sedation-related complications","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskBMI","title":"I am confident that I am able to use body mass index to assess risk of sedation-related complications","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskASA","title":"I am confident that I am able to use ASA physical classification status to assess risk of sedation-related complications","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskBMV","title":"I am confident I am able to identify in advance patients which patients might be difficult to bag-mask ventilate","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskInt","title":"I am confident I am able to identify in advance which patients might be more difficult to intubate","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskOSA","title":"I am confident that I am able to interpret results from a screening tool for Obstructive Sleep Apnoea to assess risk of sedation-related complications","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"riskAnes","title":"I am confident I am able to use information about a patient's previous anaesthesia and sedation history to assess risk for sedation-related complications","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"respHypotension","title":"I am confident I am able to respond appropriately when hypotension is detected during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"respBrady","title":"I am confident I am able to respond appropriately when bradycardia is detected during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"respHypoxia","title":"I am confident I am able to identify hypotension during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"respHypoventilation","title":"I am confident I am able to respond appropriately when under ventilation (lower than normal breath volume) is detected during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"identHypotension","title":"I am confident I am able to identify hypotension during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"identBrady","title":"I am confident I am able to identify bradycardia during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"identRr","title":"I am confident I am able to identify abnormal respiratory rate during or following an episode of sedation","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"airwayCl","title":"I am confident that I am able to apply a chin-lift correctly","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"airwayJs","title":"I am confident that I am able to apply jaw support correctly","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"airwayNpa","title":"I am confident that I am able to insert a nasopharyngeal airway correctly","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"},{"type":"rating","name":"airwayOpa","title":"I am confident that I am able to insert an oropharyngeal airway correctly","isRequired":true,"rateValues":[1,2,3,4,5,6,7],"rateMax":7,"minRateDescription":"Strongly disagree","maxRateDescription":"Strongly agree"}],"title":"Nursing Confidence in Managing Sedation Complications Scale (NC-MSCS)"},{"name":"totalRisk","elements":[{"type":"expression","name":"totalRisk","title":"Total score for risk assessment domain:","expression":"{riskCr} + {riskBMI} + {riskASA} + {riskBMV} + {riskInt}  + {riskOSA} + {riskAnes}","commentText":"Other (describe)"},{"type":"expression","name":"totalCr","title":"Total score for identifying and responding to cardiorespiratory complications domain:","expression":"{respHypotension} + {respBrady} + {respHypoxia} + {respHypoventilation} + {identHypotension} + {identBrady} + {identRr}","commentText":"Other (describe)"},{"type":"expression","name":"totalAirway","title":"Total score for risk assessment domain:","expression":"{airwayCl} + {airwayJs} + {airwayNpa} + {airwayOpa}","commentText":"Other (describe)"},{"type":"expression","name":"totalScore","title":"Total score for NC-MSCS:","expression":"{totalCr} + {totalRisk} + {totalAirway} ","commentText":"Other (describe)"}]}],"showQuestionNumbers":"off","pageNextText":"See scores"}
+var surveyJSON = {
+    "pages":[
+       {
+          "name":"page1",
+          "elements":[
+             {
+                "type":"rating",
+                "name":"riskCr",
+                "title":"I am confident that I am able to use information about cardiorespiratory reserve to assess risk for sedation-related complications",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskBMI",
+                "title":"I am confident that I am able to use body mass index to assess risk of sedation-related complications",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskASA",
+                "title":"I am confident that I am able to use ASA physical classification status to assess risk of sedation-related complications",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskBMV",
+                "title":"I am confident I am able to identify in advance patients which patients might be difficult to bag-mask ventilate",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskInt",
+                "title":"I am confident I am able to identify in advance which patients might be more difficult to intubate",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskOSA",
+                "title":"I am confident that I am able to interpret results from a screening tool for Obstructive Sleep Apnoea to assess risk of sedation-related complications",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"riskAnes",
+                "title":"I am confident I am able to use information about a patient's previous anaesthesia and sedation history to assess risk for sedation-related complications",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"respHypotension",
+                "title":"I am confident I am able to respond appropriately when hypotension is detected during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"respBrady",
+                "title":"I am confident I am able to respond appropriately when bradycardia is detected during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"respHypoxia",
+                "title":"I am confident I am able to respond appropriately when hypoxia is detected during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"respHypoventilation",
+                "title":"I am confident I am able to respond appropriately when under ventilation (lower than normal breath volume) is detected during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"identHypotension",
+                "title":"I am confident I am able to identify hypotension during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"identBrady",
+                "title":"I am confident I am able to identify bradycardia during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"identRr",
+                "title":"I am confident I am able to identify abnormal respiratory rate during or following an episode of sedation",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"airwayCl",
+                "title":"I am confident that I am able to apply a chin-lift correctly",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"airwayJs",
+                "title":"I am confident that I am able to apply jaw support correctly",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"airwayNpa",
+                "title":"I am confident that I am able to insert a nasopharyngeal airway correctly",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             },
+             {
+                "type":"rating",
+                "name":"airwayOpa",
+                "title":"I am confident that I am able to insert an oropharyngeal airway correctly",
+                "isRequired":true,
+                "rateValues":[
+                   1,
+                   2,
+                   3,
+                   4,
+                   5,
+                   6,
+                   7
+                ],
+                "rateMax":7,
+                "minRateDescription":"Strongly disagree",
+                "maxRateDescription":"Strongly agree"
+             }
+          ],
+          "title":"Nursing Confidence in Managing Sedation Complications Scale (NC-MSCS)"
+       },
+       {
+          "name":"totalRisk",
+          "elements":[
+             {
+                "type":"expression",
+                "name":"totalRisk",
+                "title":"Total score for risk assessment domain:",
+                "expression":"{riskCr} + {riskBMI} + {riskASA} + {riskBMV} + {riskInt}  + {riskOSA} + {riskAnes}",
+                "commentText":"Other (describe)"
+             },
+             {
+                "type":"expression",
+                "name":"totalCr",
+                "title":"Total score for identifying and responding to cardiorespiratory complications domain:",
+                "expression":"{respHypotension} + {respBrady} + {respHypoxia} + {respHypoventilation} + {identHypotension} + {identBrady} + {identRr}",
+                "commentText":"Other (describe)"
+             },
+             {
+                "type":"expression",
+                "name":"totalAirway",
+                "title":"Total score for risk assessment domain:",
+                "expression":"{airwayCl} + {airwayJs} + {airwayNpa} + {airwayOpa}",
+                "commentText":"Other (describe)"
+             },
+             {
+                "type":"expression",
+                "name":"totalScore",
+                "title":"Total score for NC-MSCS:",
+                "expression":"{totalCr} + {totalRisk} + {totalAirway} ",
+                "commentText":"Other (describe)"
+             }
+          ]
+       }
+    ],
+    "showQuestionNumbers":"off",
+    "pageNextText":"See scores"
+ }
 
 function sendDataToServer(survey) {
     //send Ajax request to your web server.
